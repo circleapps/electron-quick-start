e4298050b35891c66bb86f612ce4af9f6de308ad
liaowei <yyabb@outlook.com>
2019-03-17 17:46:21 +0800

    reproudce the bug.

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ function createWindow () {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    titleBarStyle: "hiddenInset",
     webPreferences: {
       nodeIntegration: true
     }
