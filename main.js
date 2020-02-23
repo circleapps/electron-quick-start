c763978c68360f2dcb3ce0a23340d78fe17ad922
edward <yyabb@outlook.com>
2020-02-23 16:33:39 +0800

    changed use case

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  //mainWindow.loadFile('index.html')
+  mainWindow.loadURL('https://youtube.com')
+    
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
